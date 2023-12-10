1c9b5b90e6164e1b0b8d83af9a2c21e6dfb20f28
Ivanidze <ivmatan@microsoft.com>
2023-12-10 00:41:55 -0800

    Switch to zoom

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -3,7 +3,6 @@ PartTwo();
 
 void PartOne()
 {
-    var ans = 0;
     var data = Parse();
     var res = Walk(data.maze, data.start);
     Console.WriteLine(res.steps - 1);
@@ -16,13 +15,12 @@ void PartOne()
     var queue = new Queue<(int x, int y)>();
     var dir = new (int x, int y)[] { (1, 0), (-1, 0), (0, 1), (0, -1) };
     var m = maze[0].Length;
+    var n = maze.Count;
     var visited = new int[maze.Count][];
     var come = new (int x, int y)[maze.Count][];
-    var noJunk = new int[maze.Count][];
-    for (int i = 0; i < maze.Count; i++)
+    for (int i = 0; i < n; i++)
     {
         visited[i] = new int[m];
-        noJunk[i] = new int[m];
         come[i] = new (int x, int y)[m];
     }
     visited[start.x][start.y] = 1;
@@ -32,7 +30,6 @@ void PartOne()
     while (true)
     {
         ans++;
-
         while (queue.Count != 0)
         {
             var cur = queue.Dequeue();
@@ -52,15 +49,18 @@ void PartOne()
         if (newQueue.Count == 0)
             break;
         (queue, newQueue) = (newQueue, queue);
-
     }
-    //Print(visited);
 
+    var zoom = new int[maze.Count * 2 + 1][];
+    for (int i = 0; i < 2 * n + 1; i++)
+        zoom[i] = new int[m * 2 + 1];
+    zoom[2 * last.x + 1][2 * last.y + 1] = -1;
+    foreach (var move in Pipe(maze[last.x][last.y]))
+        zoom[move.x + (last.x * 2) + 1][move.y + (last.y * 2) + 1] = -1;
     for (int i = 0; i < dir.Length; i++)
     {
         var tx = last.x + dir[i].x;
         var ty = last.y + dir[i].y;
-
         var val = ans;
         if (tx < 0 || ty < 0 || tx >= maze.Count || ty >= maze[0].Length)
             continue;
@@ -72,56 +72,39 @@ void PartOne()
             {
                 var prev = come[t.x][t.y];
                 visited[t.x][t.y] = -1;
-                noJunk[t.x][t.y] = -1;
+                zoom[t.x * 2 + 1][t.y * 2 + 1] = -1;
+                foreach (var move in Pipe(maze[t.x][t.y]))
+                {
+                    zoom[move.x + (t.x * 2) + 1][move.y + (t.y * 2) + 1] = -1;
+                }
                 t = prev;
                 val--;
-                // Print(visited);
             }
         }
     }
-    noJunk[last.x][last.y] = -1;
-    noJunk[start.x][start.y] = -1;
-    //Print(visited);
-    return (noJunk, ans);
-}
-void Print(int[][] visited, IList<string> maze)
-{
-    Console.WriteLine(" ");
-    for (int i = 0; i < visited.Length; i++)
-    {
-        for (int j = 0; j < visited[i].Length; j++)
-        {
-            if (visited[i][j] == -1)
-                Console.Write(maze[i][j]);
-            else if (visited[i][j] == 1000)
-                Console.Write('☺');
-            else
-                Console.Write('█');
-        }
-        Console.WriteLine();
-    }
+
+    zoom[2 * start.x + 1][2 * start.y + 1] = -1;
+    //    PrintSimple(zoom);
+    return (zoom, ans);
 }
 void PrintSimple(int[][] visited)
 {
+    Console.WriteLine();
     for (int i = 0; i < visited.Length; i++)
-    {
         Console.WriteLine(string.Join(" ", visited[i].Select(x => x < 0 ? x == -1 ? "█" : "☺" : x == 1000 ? "+" : "=")));
-        //visited.Length;
-    }
 }
 void PartTwo()
 {
     var ans = 0L;
     var data = Parse();
     var res = Walk(data.maze, data.start);
-    var n = data.maze.Count;
-    var m = data.maze[0].Length;
-    PrintSimple(res.visited);
+    var n = res.visited.Length;
+    var m = res.visited[0].Length;
+
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
             if (res.visited[i][j] != -1)
                 res.visited[i][j] = 0;
-
     for (int i = 0; i < n; i++)
     {
         if (res.visited[i][0] == 0)
@@ -136,33 +119,20 @@ void PartTwo()
         if (res.visited[n - 1][i] == 0)
             Spread(res.visited, n - 1, i);
     }
-    //Print(res.visited);
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < data.maze.Count; i++)
     {
-        var count = 0;
-        for (int j = 0; j < m; j++)
+        for (int j = 0; j < data.maze[0].Length; j++)
         {
 
-            if (res.visited[i][j] == 0)
+            if (res.visited[i * 2 + 1][j * 2 + 1] == 0)
             {
-                if (count % 2 == 1)
-                {
-                    ans++;
-                    res.visited[i][j] = 1000;
-                }
-
-            }
-            else
-            {
-                if (res.visited[i][j] == -1)
-                    if (data.maze[i][j] == 'L' || data.maze[i][j] == 'J' || data.maze[i][j] == '|')
-                        count++;
+                res.visited[i * 2 + 1][2 * j + 1] = 1000;
+                ans++;
             }
         }
     }
-    Print(res.visited, data.maze);
+    // PrintSimple(res.visited);
     Console.WriteLine(ans);
-
 }
 
 void Spread(int[][] map, int x, int y)
@@ -220,58 +190,20 @@ bool CanGo(int x, int y, int newx, int newy, IList<string> maze)
     return false;
 }
 
-List<(int x, int y)> Pipe(char pipe)
+(int x, int y) East() => (0, 1);
+(int x, int y) West() => (0, -1);
+(int x, int y) North() => (-1, 0);
+(int x, int y) South() => (1, 0);
+IList<(int x, int y)> Pipe(char pipe)
 {
-    var res = new List<(int x, int y)>();
-    if (pipe == '.')
-        return res;
-    if (pipe == '-')
+    return pipe switch
     {
-        // east
-        res.Add((0, 1));
-        // west
-        res.Add((0, -1));
-        return res;
-    }
-    if (pipe == '|')
-    {
-        //south
-        res.Add((1, 0));
-        //north
-        res.Add((-1, 0));
-        return res;
-    }
-    if (pipe == 'L')
-    {
-        // east
-        res.Add((0, 1));
-        //north
-        res.Add((-1, 0));
-        return res;
-    }
-    if (pipe == 'J')
-    {
-        //north
-        res.Add((-1, 0));
-        // west
-        res.Add((0, -1));
-        return res;
-    }
-    if (pipe == '7')
-    {
-        // west
-        res.Add((0, -1));
-        //south
-        res.Add((1, 0));
-        return res;
-    }
-    if (pipe == 'F')
-    {
-        //south
-        res.Add((1, 0));
-        // east
-        res.Add((0, 1));
-        return res;
-    }
-    return res;
+        '-' => new List<(int x, int y)>() { East(), West() },
+        '|' => new List<(int x, int y)>() { North(), South() },
+        'L' => new List<(int x, int y)>() { East(), North() },
+        'J' => new List<(int x, int y)>() { West(), North() },
+        '7' => new List<(int x, int y)>() { West(), South() },
+        'F' => new List<(int x, int y)>() { East(), South() },
+        _ => new List<(int x, int y)>()
+    };
 }
